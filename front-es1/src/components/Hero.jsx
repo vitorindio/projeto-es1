1b40e79155a6eeefc31c356ee05c8677ab523d6d
Mateus Evangelista <mateus.e.alcantara@outlook.com>
2023-11-01 18:07:11 -0300

    navbar e hero

---
+++ front-es1/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+    return (
+        <section className="container-fluid hero">
+            <div className="hero-text">
+                <h1>Sistema UwU</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, adipisci!</p>
+                <a href="" className="btn btn-success">Cadastre-se</a>
+            </div>
+        </section>
+    )
+}
