e940510403030e0c6567a8acc1c0051a630862ce
Mateus Alcantara <mateus.e.alcantara@outlook.com>
2023-11-23 21:57:52 -0300

    bd e alterações

--- front-es1/src/components/GestaoReservas.jsx
+++ front-es1/src/components/GestaoReservas.jsx
@@ -1,35 +1,88 @@
-import data from '../assets/salas.json'
+import data from "../assets/salas.json";
 
 export default function GestaoReservas() {
-
-    function gerarCardSala(sala) {
+    function gerarCardSala(sala, index) {
         return (
-            <div className={`col-md-4 ${!sala.reserva_sob_autorizacao | !sala.disponivel ? 'd-none': ''}`}>
+            <div
+                className={`col-md-4 ${
+                    !sala.reserva_sob_autorizacao | !sala.disponivel |!sala.em_analise
+                        ? "d-none"
+                        : ""
+                }`}
+            >
                 <div className="card">
                     <div className="card-body">
                         <h4 className="card-title">{sala.nome}</h4>
-                        <p className="card-text">{' '.concat(sala.recursos)}</p>
+                        <p className="card-text">{" ".concat(sala.recursos)}</p>
                     </div>
                     <ul className="list-group list-group-flush">
-                        <li className="list-group-item">Localização: {sala.localizacao}</li>
-                        <li className="list-group-item">Lotação: {sala.lotacao}</li>
-                        <li className="list-group-item">Nome do discente: {sala.responsavel}</li>
+                        <li className="list-group-item">
+                            Localização: {sala.localizacao}
+                        </li>
+                        <li className="list-group-item">
+                            Lotação: {sala.lotacao}
+                        </li>
+                        <li className="list-group-item">
+                            Nome do discente: {sala.responsavel}
+                        </li>
                     </ul>
+                    <div className="card-body">
+                        <form action="">
+                            <div className="inputs">
+                                <div class="mb-3">
+                                    <label
+                                        for={`justificativa_${index}`}
+                                        class="form-label"
+                                    >
+                                        Justificativa
+                                    </label>
+                                    <textarea
+                                        class="form-control"
+                                        id={`justificativa_${index}`}
+                                        rows="3"
+                                    ></textarea>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                     <div className="card-body d-flex">
-                        <button href="#" className="btn btn-success">Aprovar reserva</button>
-                        <button href="#" className="btn btn-danger ms-auto" disabled={!sala.disponivel}>Negar reserva</button>
+                        <button href="#" className="btn btn-success">
+                            Aprovar reserva
+                        </button>
+                        <button
+                            href="#"
+                            className="btn btn-danger ms-auto"
+                            disabled={!sala.disponivel}
+                        >
+                            Negar reserva
+                        </button>
                     </div>
                 </div>
-            </div>)
+            </div>
+        );
     }
 
-    return(
-        <section className="pt-5 mt-3 container">
-            <h2 className='text-white mb-3'>Salas a serem aprovadas</h2>
-            <div className="row g-5">
-                {data.map(gerarCardSala)}
+    function geraCardSalaOcupada(sala) {
+        return(
+            <div className="card">
+                <div className="card-body">
+                    <div className="card-title">
+                        {sala.nome}
+                    </div>
+                </div>
             </div>
-        </section>
-    )
+        )
+    }
 
+    return (
+        <>
+            <section className="pt-5 mt-3 container">
+                <h2 className="text-white mb-3">Salas a serem aprovadas</h2>
+                <div className="row g-5">{data.map(gerarCardSala)}</div>
+            </section>
+            <section className="pt-5 mt-3 container">
+                <h2 className="text-white mb-3">Salas sendo utilizadas</h2>
+            </section>
+        </>
+    );
 }
