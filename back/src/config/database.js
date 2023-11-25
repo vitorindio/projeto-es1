66b450e0106efd4c706d20459141a2487a761564
vitor <vitor.indio@owse.com.br>
2023-11-25 20:49:20 -0300

    [main-vitor] ajuste dialect

--- back/src/config/database.js
+++ back/src/config/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERX, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'sqlite',
+    dialect: 'mysql',
     port: process.env.DB_PORT,
 });
 
